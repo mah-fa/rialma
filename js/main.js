80ac9214894696accffc29c12543ed20547e77c0
Mahdi Farang <GitHub@farmah.33mail.com>
2015-09-14 17:38:33 +0430

    first

---
+++ js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    if (Modernizr.mq('(min-width: 1200px)')) {
+        jQuery(function($) {
+            $('body').panelSnap({
+              menuSelector: 'a',
+              $menu: $('#header #menu #menubar li')
+            });
+        });
+    }
+    if (Modernizr.mq('(min-width:992px) and (max-width: 1199px)')) {
+        jQuery(function($) {
+            $('body').panelSnap({
+              menuSelector: 'a',
+              $menu: $('#header #menu #menubar li')
+            });
+        });
+    }
+    if (Modernizr.mq('(min-width:768px) and (max-width: 991px)')) {
+        jQuery(function($) {
+            $('body').panelSnap({
+              menuSelector: 'a',
+              $menu: $('#header #menu #menubar li')
+            });
+        });
+    }
+    if (Modernizr.mq('(max-width: 767px)')) {}
+});
